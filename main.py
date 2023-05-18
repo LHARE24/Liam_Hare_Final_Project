0c02a058710213cf2db866a79fcd4635f94d9105
LHARE24 <L.Hare24@bcp.org>
2023-05-17 23:08:41 -0700

    doodle jump game

--- main.py
+++ main.py
@@ -11,110 +11,183 @@ Reach Goal: powerups
 
 
 
-# import libs
-import pygame as pg
+#import libraries
+import pygame
 import random
-import os
-from os import path
-# import settings 
-from settings import *
-from sprites import *
-from random import randint
-# from pg.sprite import Sprite
 
-# set up assets folders
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "img")
+#initialise pygame
+pygame.init()
 
-# create game class in order to pass properties to the sprites file
-class Game:
-    def __init__(self):
-        # init game window etc.
-        pg.init()
-        pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("my game")
-        self.clock = pg.time.Clock()
-        self.running = True 
-        print(self.screen)
-    def new(self):
-        # starting a new game
-        self.score = 0
-        self.all_sprites = pg.sprite.Group()
-        self.platforms = pg.sprite.Group()
-        self.enemies = pg.sprite.Group()
-        self.player = Player(self)
-        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
-        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
-        self.all_sprites.add(self.plat1)
+#game window dimensions
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
 
-        self.platforms.add(self.plat1)
-        
-        self.all_sprites.add(self.player)
-            
-        for plat in PLATFORM_LIST:
-            p = Platform(*plat)
-            self.all_sprites.add(p)
-            self.platforms.add(p)
-        for i in range(0,0):
-            m = Mob(20,20,(0,255,0))
-            self.all_sprites.add(m)
-            self.enemies.add(m)
-        self.run()
-    def run(self):
-        self.playing = True
-        while self.playing:
-            self.clock.tick(FPS)
-            self.events()
-            self.update()
-            self.draw()
-    
-    def events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    self.player.jump()
-    def update(self):
-        self.all_sprites.update()
-        if self.player.vel.y > 0:
-            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-            # self.score += 1
-            if hits:
-                if hits[0].variant == "disappearing":
-                    hits[0].kill()
-                elif hits[0].variant == "bouncey":
-                    self.player.pos.y = hits[0].rect.top
-                    self.player.vel.y = -PLAYER_JUMP
-                else:
-                    self.player.pos.y = hits[0].rect.top
-                    self.player.vel.y = 0
-        
-# drawing everything on the screen
-    def draw(self):
-        self.screen.fill(BLUE)
-        self.all_sprites.draw(self.screen)
-        self.draw_text(str(self.score), 30, WHITE, 725, 10)
-        pg.display.flip()
-    def draw_text(self, text, size, color, x, y):
-        font_name = pg.font.match_font('arial')
-        font = pg.font.Font(font_name, size)
-        text_surface = font.render(text, True, color)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (x,y)
-        self.screen.blit(text_surface, text_rect)
-    def get_mouse_now(self):
-        x,y = pg.mouse.get_pos()
-        return (x,y)
+#create game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Jumpy')
 
-# instantiate the game class
-g = Game()
+#set frame rate
+clock = pygame.time.Clock()
+FPS = 60
 
-# kick off the game loop
-while g.running:
-    g.new()
+#game variables
+SCROLL_THRESH = 200
+GRAVITY = 1
+MAX_PLATFORMS = 10
+scroll = 0
+bg_scroll = 0
 
-pg.quit()
+#define colours
+WHITE = (255, 255, 255)
+
+#load images
+jumpy_image = pygame.image.load('images/jumpy.png').convert_alpha()
+bg_image = pygame.image.load('images/bg.png').convert_alpha()
+platform_image = pygame.image.load('images/wood.png').convert_alpha()
+
+#function for drawing the background
+def draw_bg(bg_scroll):
+	screen.blit(bg_image, (0, 0 + bg_scroll))
+	screen.blit(bg_image, (0, -600 + bg_scroll))
+
+#player class
+class Player():
+	def __init__(self, x, y):
+		self.image = pygame.transform.scale(jumpy_image, (45, 45))
+		self.width = 25
+		self.height = 40
+		self.rect = pygame.Rect(0, 0, self.width, self.height)
+		self.rect.center = (x, y)
+		self.vel_y = 0
+		self.flip = False
+
+	def move(self):
+		#reset variables
+		scroll = 0
+		dx = 0
+		dy = 0
+
+		#process keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_a]:
+			dx = -10
+			self.flip = True
+		if key[pygame.K_d]:
+			dx = 10
+			self.flip = False
+
+		#gravity
+		self.vel_y += GRAVITY
+		dy += self.vel_y
+
+		#ensure player doesn't go off the edge of the screen
+		if self.rect.left + dx < 0:
+			dx = -self.rect.left
+		if self.rect.right + dx > SCREEN_WIDTH:
+			dx = SCREEN_WIDTH - self.rect.right
+
+
+		#check collision with platforms
+		for platform in platform_group:
+			#collision in the y direction
+			if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if above the platform
+				if self.rect.bottom < platform.rect.centery:
+					if self.vel_y > 0:
+						self.rect.bottom = platform.rect.top
+						dy = 0
+						self.vel_y = -20
+
+
+		#check collision with ground
+		if self.rect.bottom + dy > SCREEN_HEIGHT:
+			dy = 0
+			self.vel_y = -20
+
+
+		#check if the player has bounced to the top of the screen
+		if self.rect.top <= SCROLL_THRESH:
+			#if player is jumping
+			if self.vel_y < 0:
+				scroll = -dy
+
+		#update rectangle position
+		self.rect.x += dx
+		self.rect.y += dy + scroll
+
+		return scroll
+
+	def draw(self):
+		screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x - 12, self.rect.y - 5))
+		pygame.draw.rect(screen, WHITE, self.rect, 2)
+
+
+
+#platform class
+class Platform(pygame.sprite.Sprite):
+	def __init__(self, x, y, width):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(platform_image, (width, 10))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+	def update(self, scroll):
+
+		#update platform's vertical position
+		self.rect.y += scroll
+
+
+
+#player instance
+jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+
+#create sprite groups
+platform_group = pygame.sprite.Group()
+
+#create temporary platforms
+for p in range(MAX_PLATFORMS):
+	p_w = random.randint(40, 60)
+	p_x = random.randint(0, SCREEN_WIDTH - p_w)
+	p_y = p * random.randint(80, 120)
+	platform = Platform(p_x, p_y, p_w)
+	platform_group.add(platform)
+
+
+#game loop
+run = True
+while run:
+
+	clock.tick(FPS)
+
+	scroll = jumpy.move()
+
+	#draw background
+	bg_scroll += scroll
+	if bg_scroll >= 600:
+		bg_scroll = 0
+	draw_bg(bg_scroll)
+
+	#draw temporary scroll threshold
+	pygame.draw.line(screen, WHITE, (0, SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
+
+	#update platforms
+	platform_group.update(scroll)
+
+	#draw sprites
+	platform_group.draw(screen)
+	jumpy.draw()
+
+
+	#event handler
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+
+
+	#update display window
+	pygame.display.update()
+
+
+
+pygame.quit()
