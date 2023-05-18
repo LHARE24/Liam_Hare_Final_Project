7501a320c5cf55d3b07075f0b5f9475c70ff0fa8
LHARE24 <L.Hare24@bcp.org>
2023-05-17 23:40:06 -0700

    infinite platforms

--- main.py
+++ main.py
@@ -137,7 +137,9 @@ class Platform(pygame.sprite.Sprite):
 		#update platform's vertical position
 		self.rect.y += scroll
 
-
+		#check if platform has gone off the screen
+		if self.rect.top > SCREEN_HEIGHT:
+			self.kill()
 
 #player instance
 jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
@@ -145,14 +147,9 @@ jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 #create sprite groups
 platform_group = pygame.sprite.Group()
 
-#create temporary platforms
-for p in range(MAX_PLATFORMS):
-	p_w = random.randint(40, 60)
-	p_x = random.randint(0, SCREEN_WIDTH - p_w)
-	p_y = p * random.randint(80, 120)
-	platform = Platform(p_x, p_y, p_w)
-	platform_group.add(platform)
-
+#create starting platform
+platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
+platform_group.add(platform)
 
 #game loop
 run = True
@@ -168,8 +165,13 @@ while run:
 		bg_scroll = 0
 	draw_bg(bg_scroll)
 
-	#draw temporary scroll threshold
-	pygame.draw.line(screen, WHITE, (0, SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
+	#generate platforms
+	if len(platform_group) < MAX_PLATFORMS:
+		p_w = random.randint(40, 60)
+		p_x = random.randint(0, SCREEN_WIDTH - p_w)
+		p_y = platform.rect.y - random.randint(80, 120)
+		platform = Platform(p_x, p_y, p_w)
+		platform_group.add(platform)
 
 	#update platforms
 	platform_group.update(scroll)
